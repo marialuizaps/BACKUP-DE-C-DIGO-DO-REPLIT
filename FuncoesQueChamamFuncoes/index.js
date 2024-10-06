5372a72e0c92c320e46dd1a5ee6b04a0c40f56b5
maria <130366555+marialuizaps@users.noreply.github.com>
2024-10-06 18:20:22 -0300

    FuncoesqueChamamFuncoes

---
+++ FuncoesQueChamamFuncoes/index.js
@@ -0,0 +1,21 @@
+/**CRIE UM ARRAY DE OBJETOS => PESSOAS 
+*COM OS SEGUINTES ATRIBUTOS: NOME/SOBRENOME
+* CRIE UMA FUNÇÃO QUE ITERE SOBRE O ARRAY
+*E RETIRE NOMES E SOBRENOME
+* CRIE UMA VARIÁVEL COM CONTEÚDO:
+*      ->const emailBase = "@escola.pr.gov.br"
+* A PARTIR DESSES DADOS FORME UM @escola com
+* NOME.SOBRE+EMAILBASE
+* EXECUTE A FUNÇÃO...
+*/
+
+let pessoas = [
+  {nome:"joana",
+   sobrenome:"samira",
+  },
+  {nome:"samiro",
+   sobrenome:"joão",
+  },
+];
+const emailBase = "@escola.pr.gov.br";
+function email(pessoas)
