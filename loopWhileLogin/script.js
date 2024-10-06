9b012458efd8c3054ad1c3d25aa603cb1e8a17e6
maria <130366555+marialuizaps@users.noreply.github.com>
2024-10-06 18:10:06 -0300

    Loop

---
+++ loopWhileLogin/script.js
@@ -0,0 +1,46 @@
+// 1) CRIE UM LOOP QUE RODE
+// ENQUANTO O USUÁRIO NÃO ESTIVER LOGADO
+// VERIFIQUE USUÁRIO E SENHA
+// SE OS DADOS ESTIVEREM ERRADOS:
+  // INFORME O ERRO
+// SENÃO
+  // DE BOAS VINDAS
+
+
+// const usuario = "Admin"
+// const senha = "1234"
+// let estaLogado = false
+// let u, s = ""
+
+// while(!estaLogado) {
+//  u = prompt("Digite o usuário")
+//  s = prompt("Digite a senha")
+
+//   if (u == usuario && s == senha) {
+//     estaLogado = true
+//     alert("Olá, seja bem vindo ", usuario)
+// } else {
+//     alert("Usuário ou senha inválidos")
+//  }
+// }
+
+
+
+/** DESAFIO 2 */
+// CRIE UM PROGRAMA QUE PEÇA AO USUÁRIO
+// PARA ADIVINHAR MEU NÚMERO DA SORTE:
+// CRIE UM LOOP WHILE
+// MEU NÚMERO: Math.random() * 50
+// NÚMERO DE TENTATIVAS == 3 (CONDIÇÃO DE PARA)
+// CHECAR COM IF/ELSE
+
+numeroDaSorte = Math.round(Math.random() *50);
+
+console.log(numeroDaSorte);
+
+
+
+    
+
+
+
